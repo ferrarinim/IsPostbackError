9629081d51538f3ca2f31c53fa2c1566adc646b9
Martina Ferrarini <ferrarini.martina@gmail.com>
2023-04-29 16:01:37 +0200

    Add project files.

---
+++ ErrorCase/Login.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ErrorCase
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"IsPostBack = {IsPostBack}");
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"btn_Click");
+        }
+    }
+}
